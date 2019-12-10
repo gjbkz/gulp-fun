1a47902e54b44e9dadc806e6cfd992ffcd9976b7
Kei Ito <kei.itof@gmail.com>
2019-12-10 10:56:18 +0900

    chore: update deps

--- src/Parallel.ts
+++ src/Parallel.ts
@@ -4,28 +4,20 @@ import {Handler, File} from './types';
 export class Parallel extends stream.Transform {
 
     public constructor(fn: Handler) {
-        const promises: Array<Promise<Error | void>> = [];
+        const tasks: Array<Promise<void>> = [];
+        const errors: Array<Error> = [];
         super({
             objectMode: true,
             transform(file: File, _encoding, callback) {
-                promises.push(
-                    Promise.resolve()
-                    .then(() => fn(file, this))
-                    .then(() => {})
-                    .catch((error) => error)
-                );
+                tasks.push(Promise.resolve(fn(file, this))
+                .catch((error) => {
+                    errors.push(error);
+                }));
                 callback();
             },
             flush(callback) {
-                Promise.all(promises)
-                .then((errors) => {
-                    for (const error of errors) {
-                        if (error) {
-                            throw error;
-                        }
-                    }
-                    callback();
-                })
+                Promise.all(tasks)
+                .then(() => callback(errors.length === 0 ? null : new Error(errors.join('\n'))))
                 .catch(callback);
             },
         });
