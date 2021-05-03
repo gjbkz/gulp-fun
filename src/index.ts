e955ae7139f6ff0831ceacd7443d0aa032f8e2eb
Kei Ito <kei.itof@gmail.com>
2021-05-03 14:42:23 +0900

    refactor: use stream.Transform directly

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,43 @@
-import * as stream from 'stream';
+import {Transform} from 'stream';
 import type {BufferFile, DirectoryFile, NullFile, StreamFile, SymbolicFile} from 'vinyl';
+
 export type File = BufferFile | DirectoryFile | NullFile | StreamFile | SymbolicFile;
-export type Handler = (file: File, stream: stream.Transform) => Promise<void> | void;
-export class Parallel extends stream.Transform {
-
-    public constructor(fn: Handler) {
-        const tasks: Array<Promise<void>> = [];
-        const errors: Array<Error> = [];
-        super({
-            objectMode: true,
-            transform(file: File, _encoding, callback) {
-                tasks.push(
-                    (async () => await fn(file, this))()
-                    .catch((error) => {
-                        errors.push(error);
-                    }),
-                );
-                callback();
-            },
-            flush(callback) {
-                Promise.all(tasks)
-                .then(() => {
-                    if (0 < errors.length) {
-                        throw new Error(`CaughtErrors:\n${errors.join('\n')}`);
-                    }
-                    callback();
-                })
-                .catch(callback);
-            },
-        });
-    }
-
+export interface Handler {
+    (file: File, stream: Transform): Promise<void> | void,
 }
-export const parallel = (fn: Handler) => new Parallel(fn);
-export class Serial extends stream.Transform {
 
-    public constructor(fn: Handler) {
-        super({
-            objectMode: true,
-            transform(file: File, _encoding, callback) {
+export const parallel = (fn: Handler) => {
+    const tasks: Array<Promise<void>> = [];
+    const errors: Array<Error> = [];
+    return new Transform({
+        objectMode: true,
+        transform(file: File, _encoding, callback) {
+            tasks.push(
                 (async () => await fn(file, this))()
-                .then(() => callback())
-                .catch(callback);
-            },
-        });
-    }
+                .catch((error) => {
+                    errors.push(error);
+                }),
+            );
+            callback();
+        },
+        flush(callback) {
+            Promise.all(tasks)
+            .then(() => {
+                if (0 < errors.length) {
+                    throw new Error(`CaughtErrors:\n${errors.join('\n')}`);
+                }
+                callback();
+            })
+            .catch(callback);
+        },
+    });
+};
 
-}
-export const serial = (fn: Handler) => new Serial(fn);
+export const serial = (fn: Handler) => new Transform({
+    objectMode: true,
+    transform(file: File, _encoding, callback) {
+        (async () => await fn(file, this))()
+        .then(() => callback())
+        .catch(callback);
+    },
+});
